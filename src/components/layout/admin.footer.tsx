c44ae3b9afa2a865d9ea18b69abb89f38a3ead90
trinhpt <phamtientrinh4@gmail.com>
2025-01-14 17:59:09 +0700

    2

--- src/components/layout/admin.footer.tsx
+++ src/components/layout/admin.footer.tsx
@@ -7,7 +7,7 @@ const AdminFooter = () => {
     return (
         <>
             <Footer style={{ textAlign: 'center' }}>
-                Hỏi Dân IT ©{new Date().getFullYear()} Created by @hoidanit
+                BASE NEXTJS ©{new Date().getFullYear()} Created by @tienchinhjr
             </Footer>
         </>
     )
