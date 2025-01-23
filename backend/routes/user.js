7fb9093e7e3ddb7a9f11e1ecb37a3007f51ecc05
trinhdev <phamtienchinh4@gmail.com>
2025-01-24 00:27:52 +0700

    ư

--- backend/routes/user.js
+++
@@ -1,7 +0,0 @@
-const UserController = require("../controllers/userController");
-
-const router = require("express").Router();
-//Getallusers
-router.get("/", UserController.getAllUser);
-
-module.exports = router;
